1db55995e5e91f870d96eb455432953f020a3c8c
kemptwmail <kemptw@mail.uc.edu>
2025-10-29 16:20:39 -0400

    Added base ui for the pages.

--- src/main/java/com/enterprisefinancialmanagement/financialmanagespring/dto/Budget.java
+++ src/main/java/com/enterprisefinancialmanagement/financialmanagespring/dto/Budget.java
@@ -1,21 +1,19 @@
 package com.enterprisefinancialmanagement.financialmanagespring.dto;
 
-
 import lombok.Data;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
-  /**
-  * Data Transfer Object
-  * Java class for Budget 
-  * @author Melissa Manzon
-  */
-public @Data
-class  Budget {
+/**
+ * Data Transfer Object for Budget
+ */
+@Data
+public class Budget {
     private int budgetId;
+    private BigDecimal amount;
+    private String category;
     private String budgetName;
     private String description;
     private LocalDate startDate;
     private LocalDate endDate;
-
-
 }
