6088b2290f219af36094dd386c34a79a9f74baa1
Grace Holscher <111761415+JGHolscher@users.noreply.github.com>
2025-10-30 19:05:14 -0400

    IBudgetService Defined Methods

--- src/main/java/com/enterprisefinancialmanagement/financialmanagespring/service/IBudgetService.java
+++ src/main/java/com/enterprisefinancialmanagement/financialmanagespring/service/IBudgetService.java
@@ -1,6 +1,12 @@
 package com.enterprisefinancialmanagement.financialmanagespring.service;
+import java.util.List;
+
 
 import com.enterprisefinancialmanagement.financialmanagespring.dto.Budget;
 
 public interface IBudgetService {
+    Budget createBudget(Budget budget);
+    Budget getBudgetById(String id);
+    List<Budget> getAllBudgets();
+    boolean deleteBudget(String id);
 }
