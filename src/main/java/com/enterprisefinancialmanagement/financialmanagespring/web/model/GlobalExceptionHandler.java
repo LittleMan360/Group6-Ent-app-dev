bcd4cd9f66a328074c7071282d24a6e6db65ff2d
Harshpatel0815 <patelhj@mail.uc.edu>
2025-11-02 18:09:14 -0500

    add global exception handler and null-safety improvements

---
+++ src/main/java/com/enterprisefinancialmanagement/financialmanagespring/web/model/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.enterprisefinancialmanagement.financialmanagespring.web.model;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleGenericException(Exception ex, Model model) {
+        model.addAttribute("errorMessage", "An unexpected error occurred: " + ex.getMessage());
+        return "error";
+    }
+}
